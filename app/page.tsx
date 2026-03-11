e0c64a39c16705a6b267146b738dcf1b36fb5e50
martinez-frank <martinez.frank.v@gmail.com>
2026-03-10 20:55:21 -0700

    Fix story page navigation and homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import Hero from "../components/Hero";
-import StorySection from "../components/StorySection";
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <StorySection />
-    </>
-  );
+  return <Hero />;
 }
